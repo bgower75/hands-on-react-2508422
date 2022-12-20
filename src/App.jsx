fff4b8a19b5c13e7fb71517cb55886e4f0de2fdb
Belinda Gower <25243519+bgower75@users.noreply.github.com>
2022-12-20 10:40:46 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <h1>Meet the StarGazers</h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
