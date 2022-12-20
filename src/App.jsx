a3c26b27bef029252c489ad937748b2ad433774c
Belinda Gower <25243519+bgower75@users.noreply.github.com>
2022-12-20 11:51:29 +0000

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 function App() {
+  const name = 'StarGazers'
+  let color = "purple", buttonText="black", buttonColor="teal"
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the StarGazers</h1>
+          <h1>Meet the <i style={{color}}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert("Hi there!")} style={{backgroundColor:buttonColor,color:buttonText}}>Click Me</button>
         </hgroup>
       </article>
     </div>
